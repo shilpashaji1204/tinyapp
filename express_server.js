62468bb227202635834f5179c835af0267174c5b
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-20 23:46:33 -0400

    updated

--- express_server.js
+++ express_server.js
@@ -2,40 +2,59 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");  // Set EJs as the templating engine
 
+// Sample database of shortened URLs
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
+    "b2xVn2": "http://www.lighthouselabs.ca",       
   "9sm5xK": "http://www.google.com"
 }; 
 
+// Middleware to parse the body of incoming requests
 app.use(express.urlencoded({ extended: true }));
-
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+app.post("/urls", (req, res) => {
+    const longURL = req.body.longURL; // retrieve the URL from the form data
+    const shortURL = generateRandomString(); // generate a short URL for the long URL
+    urlDatabase[shortURL] = longURL; // add the new URL to the database
+    res.redirect(`/urls/${shortURL}`); // redirect to the page for the new URL
   });
 
+
+// Route to display the form for creating a new shortened URL
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");  // Render the EJS template
+  });
+
+  // Route to display the details of a specific shortened URL
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
-  });
+    res.render("urls_show", templateVars);  // Render the EJS template with data
+});
 
+// Route to display all shortened URLs in the database
   app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+    res.render("urls_index", templateVars);  // Render the EJS template with data
   });
 
+  // Root route that simply displays "Hello!"
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
+
+// Route that returns the JSON representation of the database
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
+// Route that displays "Hello World" in bold
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+// Start the server and listen for incoming requests
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
