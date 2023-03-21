617de80d8b6e434ddefe2e4e698b3686e8cad1ba
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-20 22:03:59 -0400

    Added html and css

--- express_server.js
+++ express_server.js
@@ -7,7 +7,13 @@ app.set("view engine", "ejs");
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+}; 
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
 
 app.get("/", (req, res) => {
     res.send("Hello!");
